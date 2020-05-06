7dc4e880c11cdebd2d3a68ae74e56548295ef815
o1059de4649 <40345411+o1059de4649@users.noreply.github.com>
2020-05-06 21:10:39 +0900

    石メッシュの追加

---
+++ Assets/Resources/Scripts/System/RotateScreenScene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScreenScene : MonoBehaviour
+{
+    public enum ScreenType 
+    {
+        Portrait,
+        PortraitUpsideDown,
+        LandscapeLeft,
+        LandscapeRight
+    }
+    public ScreenType screenType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScreenType();
+    }
+
+    /// <summary>
+    /// スクリーン
+    /// </summary>
+    void SetScreenType() 
+    {
+        switch (screenType)
+        {
+            case ScreenType.Portrait:
+                Screen.orientation = ScreenOrientation.Portrait;
+                break;
+            case ScreenType.PortraitUpsideDown:
+                Screen.orientation = ScreenOrientation.PortraitUpsideDown;
+                break;
+            case ScreenType.LandscapeLeft:
+                Screen.orientation = ScreenOrientation.LandscapeLeft;
+                break;
+            case ScreenType.LandscapeRight:
+                Screen.orientation = ScreenOrientation.LandscapeRight;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
