b98658a1e86da8c78cd51bd87bbc24bd9b4b243b
o1059de4649 <40345411+o1059de4649@users.noreply.github.com>
2020-05-24 20:27:47 +0900

    マテリアルのプッシュ

---
+++ Assets/Resources/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [field: SerializeField]
+    [field: RenameField("遷移先シーンのパス")]
+    string ScenePath { get; set; }
+
+    /// <summary>
+    /// ボタン専用イベントハンドラ
+    /// </summary>
+    private void LoadScene() 
+    {
+        SceneManager.LoadScene(ScenePath);
+    }
+
+    /// <summary>
+    /// 全クラスで使用可能なロードメソッド
+    /// </summary>
+    /// <param name="path"></param>
+    static public void LoadSceneStatic(string path) 
+    {
+        SceneManager.LoadScene(path);
+    }
+}
